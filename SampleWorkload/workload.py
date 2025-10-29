a7afdba68c77363d5f1baf8bd6b22bd4dd4c2331
vsamokhin3 <vs2048@outlook.com>
2025-10-29 09:20:44 -0400

    Set up framework
    Created the main file, the workers, and a sample workload for testing.

---
+++ SampleWorkload/workload.py
@@ -0,0 +1,10 @@
+# This is a sample workload to test SimuBatchr
+#
+# It just spins for a random amount of time
+
+def RunTest(identifier, nTests):
+    pass
+
+
+if __name__ == "__main__":
+    print("Running workload\n")
